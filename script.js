6eecb93a74d10728f303bd9f5309fb4821642912
cho-wonjun <wonjun2004@gmail.com>
2022-11-03 23:19:22 -0700

    1

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 let x = 0; 
+
+
+async function request() {
+    try {
+        console.log("hi");
+        await fetch("something.com");
+        console.log("bye");
+        
+    } catch (error) {
+        console.log(error, "bored");
+    }
+}
+
+request();
+
+console.log("i am here");
